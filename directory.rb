57f4403d72ed7f80c2998c8be7e9d57e7fbb8c66
Anna Cavalla <anna.cavalla@gmail.com>
2020-12-17 19:55:21 +0000

    Added choosing a starting letter and a length specification; altered each_with_index to a while loop with an if clause and and i index counter

--- directory.rb
+++ directory.rb
@@ -13,14 +13,38 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # Create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #Get another name from the user
+    name = gets.chomp
+  end
+  puts "What letter do you want?"
+  letter = gets.chomp
+  # return the array of students
+  return students, letter
+end
+
 def print_header
   puts "The students of Villains' Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do | student |
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print(students, letter)
+  i = 0
+  while i < students.length
+    if students[i][:name][0] == letter && students[i][:name].length < 12
+      puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    end
+    i += 1
   end
 end
 
@@ -28,8 +52,9 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-#Nothing happens until we call the methods
+students, letter = input_students
+
 print_header
-print(students)
+print(students, letter)
 print_footer(students)
 
