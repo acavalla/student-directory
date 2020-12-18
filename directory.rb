5dd06605e0ddaf9b54773c22fa6891ef954bc6ef
Anna Cavalla <anna.cavalla@gmail.com>
2020-12-18 17:47:29 +0000

    .each notation refactored into a while loop

--- directory.rb
+++ directory.rb
@@ -1,18 +1,3 @@
-#first we list the students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Kreuger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -27,10 +12,8 @@ def input_students
     #Get another name from the user
     name = gets.chomp
   end
-  puts "What letter do you want?"
-  letter = gets.chomp
   # return the array of students
-  return students, letter
+  return students
 end
 
 def print_header
@@ -38,9 +21,11 @@ def print_header
   puts "-------------"
 end
 
-def print(students, letter)
+def print(students)
   i = 0
-  while i < students.length
+  puts "What letter do you want?"
+  letter = gets.chomp
+  while i < students.length do
     if students[i][:name][0] == letter && students[i][:name].length < 12
       puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
     end
@@ -52,9 +37,9 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students, letter = input_students
-
+students = input_students
+#sport = input_sport
 print_header
-print(students, letter)
+print(students)
 print_footer(students)
 
